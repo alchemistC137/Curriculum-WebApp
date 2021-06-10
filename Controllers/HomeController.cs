60b48c51dee6d19bd57edf1d20d8638d945ebd41
alchemist <markomemba@gmail.com>
2021-06-10 18:55:07 +0200

    logkit y datakit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Curriculum_WebApp.Models;
-using watools_log;
+
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,28 +1,26 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Curriculum_WebApp.Models;
-using watools_log;
-using Microsoft.Extensions.Configuration;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
-using System;
-using System.Data.Common;
-using Microsoft.Extensions.Options;
-using MySqlConnector;
 using Microsoft.AspNetCore.Http;
+using DataKit;
 
 namespace Curriculum_WebApp.Controllers
 {
     public class LoginController : Controller
     {
+
+        PoolConnection pc = new PoolConnection();
+
         [HttpPost]
         public void checkCredentials(IFormCollection fc)
         {
 
+
             UserModel logedUser = new UserModel();
             logedUser.user = fc["mail"].ToString();
             logedUser.pass = fc["pass"].ToString();
+
+            pc.saludo();
+
         }
     }
 
